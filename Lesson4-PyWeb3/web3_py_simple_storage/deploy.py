1d3e8abfc0fbc866b1e2077a1b6c96699f2e7e31
ikcoin <ikcoin.kb@protonmail.com>
2021-11-23 17:38:30 +0100

    Deploy python script

---
+++ Lesson4-PyWeb3/web3_py_simple_storage/deploy.py
@@ -0,0 +1,68 @@
+from solcx import compile_standard, install_solc
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+import json
+
+# load .env
+load_dotenv()
+
+# install_solc("0.8.0")
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# Compile the Smart Contracts
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.8.0",
+)
+
+with open("compiled_sol.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get ABI
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+
+# Ganache blockchain connection
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x5cAAd74bD53A90Dc6BbC6644A97812a5A7891333"
+private_key = os.getenv("PRIVATE_KEY")
+
+# contract creation
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# get account current nonce
+nonce = w3.eth.getTransactionCount(my_address)
+
+# transaction built
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+
+signed_transaction = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key
+)
+
+tx_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
