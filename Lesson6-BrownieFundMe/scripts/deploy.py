572e7f92e77f9f48a3a636918fbd666666a9dbce
ikcoin <ikcoin.kb@protonmail.com>
2021-11-26 16:29:28 +0100

    Fund & withdeaw scrip

--- Lesson6-BrownieFundMe/scripts/deploy.py
+++ Lesson6-BrownieFundMe/scripts/deploy.py
@@ -1,21 +1,21 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIROMENTS,
+)
+from web3 import Web3
 
 
 def deploy_fund_me():
     account = get_account()
 
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIROMENTS:
         price_feed_address = config["netwoks"][network.show_active()][
             "eth_usd_price_feed"
         ]
     else:  # mock
-        print("Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000000, {"from": account}
-        )
-        print("MockV3Aggregator deployed at address: ", mock_aggregator.address)
-        price_feed_address = mock_aggregator.address
+        price_feed_address = deploy_mocks()
 
     fund_me = FundMe.deploy(
         price_feed_address,
@@ -23,6 +23,7 @@ def deploy_fund_me():
         publish_source=config["networks"][network.show_active()]["verify"],
     )
     print("FundMe deployed on addres: ", fund_me.address)
+    return fund_me
 
 
 def main():
