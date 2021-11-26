ec2ba927eac1ce142d908358cd39d2f68fe25d90
ikcoin <ikcoin.kb@protonmail.com>
2021-11-26 16:28:39 +0100

    getEntranceFee function

--- Lesson6-BrownieFundMe/contracts/FundMe.sol
+++ Lesson6-BrownieFundMe/contracts/FundMe.sol
@@ -66,4 +66,11 @@ contract FundMe {
         }
         funders = new address[](0);
     }
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 minimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (minimumUSD * price) / precision;
+    }
 }
