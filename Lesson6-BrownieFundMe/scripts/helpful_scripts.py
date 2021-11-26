3d61410c647ddbd77ea69c7e7717d7774fcbabf7
ikcoin <ikcoin.kb@protonmail.com>
2021-11-26 16:31:06 +0100

    Local development networks constant

--- Lesson6-BrownieFundMe/scripts/helpful_scripts.py
+++ Lesson6-BrownieFundMe/scripts/helpful_scripts.py
@@ -1,8 +1,30 @@
-from brownie import network, accounts, config
+from brownie import network, accounts, config, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-local"]
 
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    account = get_account()
+
+    print("Deploying Mocks...")
+    print("----------", len(MockV3Aggregator))
+    if len(MockV3Aggregator) <= 0:  # Contract not deployed yet
+        MockV3Aggregator.deploy(
+            DECIMALS,
+            STARTING_PRICE,
+            {"from": account},  # Web3.toWei(STARTING_PRICE, "ether")
+        )
+        print("MockV3Aggregator deployed at address: ", MockV3Aggregator[-1].address)
+
+    return MockV3Aggregator[-1].address
