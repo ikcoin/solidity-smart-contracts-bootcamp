0a92d275ac50ea4048d3a9c1139a88633ac150f9
ikcoin <ikcoin.kb@protonmail.com>
2021-12-02 17:25:30 +0100

    defined collateral ammount to withdraw var

--- Lesson10-DeFi-Aave/scripts/withdraw_weth.py
+++ Lesson10-DeFi-Aave/scripts/withdraw_weth.py
@@ -3,15 +3,15 @@ from scripts.helpful_scripts import get_account
 from web3 import Web3
 
 
-def withdrawy_weth():
+def withdrawy_weth(collateral):
 
     account = get_account()
 
     # To interact with the WETH Contract, we need the ABI and the Address
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
-    tx = weth.withdraw(Web3.toWei(0.1, "ether"), {f"from": account})
+    tx = weth.withdraw(Web3.toWei(collateral, "ether"), {f"from": account})
     tx.wait(1)
-    print("Received 0.1 ETH")
+    print("Received ", collateral, " ETH")
 
 
 def main():
